bccded45342bcdc5a9176539270ae1efeef3d319
Matthieu Vergne <matthieu.vergne@gmail.com>
2016-09-05 03:59:17 +0900

    Add a factory method to create a kana from romaji.

--- src/main/java/fr/vergne/japdict/jap/KanaFactory.java
+++ src/main/java/fr/vergne/japdict/jap/KanaFactory.java
@@ -5,6 +5,282 @@ import java.util.List;
 public class KanaFactory {
 
 	/**
+	 * This method provides a simple way to obtain a kana from ASCII characters.
+	 * The romanization used is inspired from the Hepburn romanization, although
+	 * not strictly similar to avoid ambiguity (e.g. 'お' and 'を' are both
+	 * romanized as 'o' in the Hepburn's version). Because we deal with single
+	 * kanas, a romaji for a combination (e.g. 'kya' for 'きゃ') is invalid. Any
+	 * invalid romaji lead to throwing an exception. The complete mapping is
+	 * described below and is case insensitive.<br/>
+	 * <br/>
+	 * <b>BASE</b>
+	 * <ul>
+	 * <li>あ ア a</li>
+	 * <li>い イ i</li>
+	 * <li>う ウ u</li>
+	 * <li>え エ e</li>
+	 * <li>お オ o</li>
+	 * <li>か カ ka</li>
+	 * <li>き キ ki</li>
+	 * <li>く ク ku</li>
+	 * <li>け ケ ke</li>
+	 * <li>こ コ ko</li>
+	 * <li>さ サ sa</li>
+	 * <li>し シ shi</li>
+	 * <li>す ス su</li>
+	 * <li>せ セ se</li>
+	 * <li>そ ソ so</li>
+	 * <li>た タ ta</li>
+	 * <li>ち チ chi</li>
+	 * <li>つ ツ tsu</li>
+	 * <li>て テ te</li>
+	 * <li>と ト to</li>
+	 * <li>な ナ na</li>
+	 * <li>に ニ ni</li>
+	 * <li>ぬ ヌ nu</li>
+	 * <li>ね ネ ne</li>
+	 * <li>の ノ no</li>
+	 * <li>は ハ ha</li>
+	 * <li>ひ ヒ hi</li>
+	 * <li>ふ フ fu (or hu)</li>
+	 * <li>へ ヘ he</li>
+	 * <li>ほ ホ ho</li>
+	 * <li>ま マ ma</li>
+	 * <li>み ミ mi</li>
+	 * <li>む ム mu</li>
+	 * <li>め メ me</li>
+	 * <li>も モ mo</li>
+	 * <li>や ヤ ya</li>
+	 * <li>ゆ ユ yu</li>
+	 * <li>よ ヨ yo</li>
+	 * <li>ら ラ ra</li>
+	 * <li>り リ ri</li>
+	 * <li>る ル ru</li>
+	 * <li>れ レ re</li>
+	 * <li>ろ ロ ro</li>
+	 * <li>わ ワ wa</li>
+	 * <li>を ヲ wo</li>
+	 * <li>ん ン n</li>
+	 * </ul>
+	 * <br/>
+	 * <b>OLD</b>
+	 * <ul>
+	 * <li>ゐ ヰ wi</li>
+	 * <li>ゑ ヱ we</li>
+	 * </ul>
+	 * <br/>
+	 * <b>TENTEN</b>
+	 * <ul>
+	 * <li>が ガ ga</li>
+	 * <li>ぎ ギ gi</li>
+	 * <li>ぐ グ gu</li>
+	 * <li>げ ゲ ge</li>
+	 * <li>ご ゴ go</li>
+	 * <li>ざ ザ za</li>
+	 * <li>じ ジ ji</li>
+	 * <li>ず ズ zu</li>
+	 * <li>ぜ ゼ ze</li>
+	 * <li>ぞ ゾ zo</li>
+	 * <li>だ ダ da</li>
+	 * <li>ぢ ヂ dji</li>
+	 * <li>づ ヅ dzu</li>
+	 * <li>で デ de</li>
+	 * <li>ど ド do</li>
+	 * <li>ば バ ba</li>
+	 * <li>び ビ bi</li>
+	 * <li>ぶ ブ bu</li>
+	 * <li>べ ベ be</li>
+	 * <li>ぼ ボ bo</li>
+	 * <li>ゔ ヴ vu</li>
+	 * </ul>
+	 * <br/>
+	 * <b>MARU</b>
+	 * <ul>
+	 * <li>ぱ パ pa</li>
+	 * <li>ぴ ピ pi</li>
+	 * <li>ぷ プ pu</li>
+	 * <li>ぺ ペ pe</li>
+	 * <li>ぽ ポ po</li>
+	 * </ul>
+	 * <br/>
+	 * <b>SMALL</b><br/>
+	 * (Like BASE but preceded with + sign)
+	 * <ul>
+	 * <li>ぁ ァ +a</li>
+	 * <li>ぃ ィ +i</li>
+	 * <li>ぅ ゥ +u</li>
+	 * <li>ぇ ェ +e</li>
+	 * <li>ぉ ォ +o</li>
+	 * <li>ゕ ヵ +ka</li>
+	 * <li>ゖ ヶ +ke</li>
+	 * <li>っ ッ +tsu</li>
+	 * <li>ゃ ャ +ya</li>
+	 * <li>ゅ ュ +yu</li>
+	 * <li>ょ ョ +yo</li>
+	 * <li>ゎ ヮ +wa</li>
+	 * </ul>
+	 * 
+	 * @param romaji
+	 *            the romaji writing of the kana
+	 * @param type
+	 *            the {@link KanaType} wanted
+	 * @return the {@link JapCharacter} of the kana corresponding to the romaji
+	 *         and {@link KanaType} requested
+	 */
+	public JapCharacter createFromRomaji(final String romaji,
+			final KanaType type) {
+		if (romaji == null) {
+			throw new NullPointerException("No romaji provided");
+		} else if (romaji.isEmpty()) {
+			throw new IllegalArgumentException("No romaji provided");
+		} else if (type == null) {
+			throw new NullPointerException("No type provided");
+		} else {
+			KanaVariant variant;
+			char[] characters = romaji.toLowerCase().toCharArray();
+			if (characters[0] == '+') {
+				variant = KanaVariant.SMALL;
+				characters[0] = ' ';
+			} else if (characters[0] == 'p') {
+				variant = KanaVariant.MARU;
+				characters[0] = 'h';
+			} else if (characters[0] == 'v') {
+				variant = KanaVariant.TENTEN;
+				characters[0] = ' ';
+			} else if (characters[0] == 'g') {
+				variant = KanaVariant.TENTEN;
+				characters[0] = 'k';
+			} else if (characters[0] == 'z' || characters[0] == 'j') {
+				variant = KanaVariant.TENTEN;
+				characters[0] = 's';
+			} else if (characters[0] == 'd') {
+				variant = KanaVariant.TENTEN;
+				characters[0] = 't';
+			} else if (characters[0] == 'b') {
+				variant = KanaVariant.TENTEN;
+				characters[0] = 'h';
+			} else {
+				variant = KanaVariant.NONE;
+			}
+
+			String base = new String(characters).replaceAll(" ", "");
+			base = base.equals("tji") ? "chi" : base;
+			base = base.equals("si") ? "shi" : base;
+			base = base.equals("tzu") ? "tsu" : base;
+			char code;
+			if (base.equals("a")) {
+				code = 'あ';
+			} else if (base.equals("i")) {
+				code = 'い';
+			} else if (base.equals("u")) {
+				code = 'う';
+			} else if (base.equals("e")) {
+				code = 'え';
+			} else if (base.equals("o")) {
+				code = 'お';
+			} else if (base.equals("ka")) {
+				code = 'か';
+			} else if (base.equals("ki")) {
+				code = 'き';
+			} else if (base.equals("ku")) {
+				code = 'く';
+			} else if (base.equals("ke")) {
+				code = 'け';
+			} else if (base.equals("ko")) {
+				code = 'こ';
+			} else if (base.equals("sa")) {
+				code = 'さ';
+			} else if (base.equals("shi")) {
+				code = 'し';
+			} else if (base.equals("su")) {
+				code = 'す';
+			} else if (base.equals("se")) {
+				code = 'せ';
+			} else if (base.equals("so")) {
+				code = 'そ';
+			} else if (base.equals("ta")) {
+				code = 'た';
+			} else if (base.equals("chi")) {
+				code = 'ち';
+			} else if (base.equals("tsu")) {
+				code = 'つ';
+			} else if (base.equals("te")) {
+				code = 'て';
+			} else if (base.equals("to")) {
+				code = 'と';
+			} else if (base.equals("na")) {
+				code = 'な';
+			} else if (base.equals("ni")) {
+				code = 'に';
+			} else if (base.equals("nu")) {
+				code = 'ぬ';
+			} else if (base.equals("ne")) {
+				code = 'ね';
+			} else if (base.equals("no")) {
+				code = 'の';
+			} else if (base.equals("ha")) {
+				code = 'は';
+			} else if (base.equals("hi")) {
+				code = 'ひ';
+			} else if (base.equals("hu")) {
+				code = 'ふ';
+			} else if (base.equals("fu")) {
+				code = 'ふ';
+			} else if (base.equals("he")) {
+				code = 'へ';
+			} else if (base.equals("ho")) {
+				code = 'ほ';
+			} else if (base.equals("ma")) {
+				code = 'ま';
+			} else if (base.equals("mi")) {
+				code = 'み';
+			} else if (base.equals("mu")) {
+				code = 'む';
+			} else if (base.equals("me")) {
+				code = 'め';
+			} else if (base.equals("mo")) {
+				code = 'も';
+			} else if (base.equals("ya")) {
+				code = 'や';
+			} else if (base.equals("yu")) {
+				code = 'ゆ';
+			} else if (base.equals("yo")) {
+				code = 'よ';
+			} else if (base.equals("ra")) {
+				code = 'ら';
+			} else if (base.equals("ri")) {
+				code = 'り';
+			} else if (base.equals("ru")) {
+				code = 'る';
+			} else if (base.equals("re")) {
+				code = 'れ';
+			} else if (base.equals("ro")) {
+				code = 'ろ';
+			} else if (base.equals("wa")) {
+				code = 'わ';
+			} else if (base.equals("wo")) {
+				code = 'を';
+			} else if (base.equals("n")) {
+				code = 'ん';
+			} else if (base.equals("wi")) {
+				code = 'ゐ';
+			} else if (base.equals("we")) {
+				code = 'ゑ';
+			} else {
+				code = 0;
+			}
+			JapCharacter character = transform(new JapCharacter(code), type,
+					variant);
+			if (character == null) {
+				throw new IllegalArgumentException(base + " is not a valid "
+						+ type);
+			} else {
+				return character;
+			}
+		}
+	}
+
+	/**
 	 * 
 	 * @param character
 	 *            the {@link JapCharacter} to start from
--- src/test/java/fr/vergne/japdict/jap/KanaFactoryTest.java
+++ src/test/java/fr/vergne/japdict/jap/KanaFactoryTest.java
@@ -2,6 +2,13 @@ package fr.vergne.japdict.jap;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.junit.Test;
 
 /**
@@ -12,6 +19,370 @@ import org.junit.Test;
  * 
  */
 public class KanaFactoryTest {
+
+	@Test
+	public void testCreateFromRomajiReturnsProperHiragana() {
+		KanaFactory factory = new KanaFactory();
+		Map<String, Character> expected = new HashMap<>();
+		expected.put("a", 'あ');
+		expected.put("i", 'い');
+		expected.put("u", 'う');
+		expected.put("e", 'え');
+		expected.put("o", 'お');
+		expected.put("ka", 'か');
+		expected.put("ki", 'き');
+		expected.put("ku", 'く');
+		expected.put("ke", 'け');
+		expected.put("ko", 'こ');
+		expected.put("sa", 'さ');
+		expected.put("shi", 'し');
+		expected.put("su", 'す');
+		expected.put("se", 'せ');
+		expected.put("so", 'そ');
+		expected.put("ta", 'た');
+		expected.put("chi", 'ち');
+		expected.put("tsu", 'つ');
+		expected.put("te", 'て');
+		expected.put("to", 'と');
+		expected.put("na", 'な');
+		expected.put("ni", 'に');
+		expected.put("nu", 'ぬ');
+		expected.put("ne", 'ね');
+		expected.put("no", 'の');
+		expected.put("ha", 'は');
+		expected.put("hi", 'ひ');
+		expected.put("hu", 'ふ');
+		expected.put("fu", 'ふ');
+		expected.put("he", 'へ');
+		expected.put("ho", 'ほ');
+		expected.put("ma", 'ま');
+		expected.put("mi", 'み');
+		expected.put("mu", 'む');
+		expected.put("me", 'め');
+		expected.put("mo", 'も');
+		expected.put("ya", 'や');
+		expected.put("yu", 'ゆ');
+		expected.put("yo", 'よ');
+		expected.put("ra", 'ら');
+		expected.put("ri", 'り');
+		expected.put("ru", 'る');
+		expected.put("re", 'れ');
+		expected.put("ro", 'ろ');
+		expected.put("wa", 'わ');
+		expected.put("wo", 'を');
+		expected.put("n", 'ん');
+
+		expected.put("wi", 'ゐ');
+		expected.put("we", 'ゑ');
+
+		expected.put("vu", 'ゔ');
+		expected.put("ga", 'が');
+		expected.put("gi", 'ぎ');
+		expected.put("gu", 'ぐ');
+		expected.put("ge", 'げ');
+		expected.put("go", 'ご');
+		expected.put("za", 'ざ');
+		expected.put("ji", 'じ');
+		expected.put("zu", 'ず');
+		expected.put("ze", 'ぜ');
+		expected.put("zo", 'ぞ');
+		expected.put("da", 'だ');
+		expected.put("dji", 'ぢ');
+		expected.put("dzu", 'づ');
+		expected.put("de", 'で');
+		expected.put("do", 'ど');
+		expected.put("ba", 'ば');
+		expected.put("bi", 'び');
+		expected.put("bu", 'ぶ');
+		expected.put("be", 'べ');
+		expected.put("bo", 'ぼ');
+
+		expected.put("pa", 'ぱ');
+		expected.put("pi", 'ぴ');
+		expected.put("pu", 'ぷ');
+		expected.put("pe", 'ぺ');
+		expected.put("po", 'ぽ');
+
+		expected.put("+a", 'ぁ');
+		expected.put("+i", 'ぃ');
+		expected.put("+u", 'ぅ');
+		expected.put("+e", 'ぇ');
+		expected.put("+o", 'ぉ');
+		expected.put("+ka", 'ゕ');
+		expected.put("+ke", 'ゖ');
+		expected.put("+tsu", 'っ');
+		expected.put("+ya", 'ゃ');
+		expected.put("+yu", 'ゅ');
+		expected.put("+yo", 'ょ');
+		expected.put("+wa", 'ゎ');
+
+		for (Entry<String, Character> entry : expected.entrySet()) {
+			String romaji = entry.getKey();
+			char jap = entry.getValue();
+			assertEquals("For " + romaji + ": ", "" + jap, factory
+					.createFromRomaji(romaji, KanaType.HIRAGANA).toString());
+		}
+	}
+
+	@Test
+	public void testCreateFromRomajiReturnsProperKatakana() {
+		KanaFactory factory = new KanaFactory();
+		Map<String, Character> expected = new HashMap<>();
+		expected.put("a", 'ア');
+		expected.put("i", 'イ');
+		expected.put("u", 'ウ');
+		expected.put("e", 'エ');
+		expected.put("o", 'オ');
+		expected.put("ka", 'カ');
+		expected.put("ki", 'キ');
+		expected.put("ku", 'ク');
+		expected.put("ke", 'ケ');
+		expected.put("ko", 'コ');
+		expected.put("sa", 'サ');
+		expected.put("shi", 'シ');
+		expected.put("su", 'ス');
+		expected.put("se", 'セ');
+		expected.put("so", 'ソ');
+		expected.put("ta", 'タ');
+		expected.put("chi", 'チ');
+		expected.put("tsu", 'ツ');
+		expected.put("te", 'テ');
+		expected.put("to", 'ト');
+		expected.put("na", 'ナ');
+		expected.put("ni", 'ニ');
+		expected.put("nu", 'ヌ');
+		expected.put("ne", 'ネ');
+		expected.put("no", 'ノ');
+		expected.put("ha", 'ハ');
+		expected.put("hi", 'ヒ');
+		expected.put("hu", 'フ');
+		expected.put("fu", 'フ');
+		expected.put("he", 'ヘ');
+		expected.put("ho", 'ホ');
+		expected.put("ma", 'マ');
+		expected.put("mi", 'ミ');
+		expected.put("mu", 'ム');
+		expected.put("me", 'メ');
+		expected.put("mo", 'モ');
+		expected.put("ya", 'ヤ');
+		expected.put("yu", 'ユ');
+		expected.put("yo", 'ヨ');
+		expected.put("ra", 'ラ');
+		expected.put("ri", 'リ');
+		expected.put("ru", 'ル');
+		expected.put("re", 'レ');
+		expected.put("ro", 'ロ');
+		expected.put("wa", 'ワ');
+		expected.put("wo", 'ヲ');
+		expected.put("n", 'ン');
+
+		expected.put("wi", 'ヰ');
+		expected.put("we", 'ヱ');
+
+		expected.put("vu", 'ヴ');
+		expected.put("ga", 'ガ');
+		expected.put("gi", 'ギ');
+		expected.put("gu", 'グ');
+		expected.put("ge", 'ゲ');
+		expected.put("go", 'ゴ');
+		expected.put("za", 'ザ');
+		expected.put("ji", 'ジ');
+		expected.put("zu", 'ズ');
+		expected.put("ze", 'ゼ');
+		expected.put("zo", 'ゾ');
+		expected.put("da", 'ダ');
+		expected.put("dji", 'ヂ');
+		expected.put("dzu", 'ヅ');
+		expected.put("de", 'デ');
+		expected.put("do", 'ド');
+		expected.put("ba", 'バ');
+		expected.put("bi", 'ビ');
+		expected.put("bu", 'ブ');
+		expected.put("be", 'ベ');
+		expected.put("bo", 'ボ');
+
+		expected.put("pa", 'パ');
+		expected.put("pi", 'ピ');
+		expected.put("pu", 'プ');
+		expected.put("pe", 'ペ');
+		expected.put("po", 'ポ');
+
+		expected.put("+a", 'ァ');
+		expected.put("+i", 'ィ');
+		expected.put("+u", 'ゥ');
+		expected.put("+e", 'ェ');
+		expected.put("+o", 'ォ');
+		expected.put("+ka", 'ヵ');
+		expected.put("+ke", 'ヶ');
+		expected.put("+tsu", 'ッ');
+		expected.put("+ya", 'ャ');
+		expected.put("+yu", 'ュ');
+		expected.put("+yo", 'ョ');
+		expected.put("+wa", 'ヮ');
+
+		for (Entry<String, Character> entry : expected.entrySet()) {
+			String romaji = entry.getKey();
+			char jap = entry.getValue();
+			assertEquals("For " + romaji + ": ", "" + jap, factory
+					.createFromRomaji(romaji, KanaType.KATAKANA).toString());
+		}
+	}
+
+	@Test
+	public void testCreateFromRomajiIsCaseInsensitive() {
+		KanaFactory factory = new KanaFactory();
+		Collection<String> list = new LinkedList<>();
+		list.add("a");
+		list.add("i");
+		list.add("u");
+		list.add("e");
+		list.add("o");
+		list.add("ka");
+		list.add("ki");
+		list.add("ku");
+		list.add("ke");
+		list.add("ko");
+		list.add("sa");
+		list.add("shi");
+		list.add("su");
+		list.add("se");
+		list.add("so");
+		list.add("ta");
+		list.add("chi");
+		list.add("tsu");
+		list.add("te");
+		list.add("to");
+		list.add("na");
+		list.add("ni");
+		list.add("nu");
+		list.add("ne");
+		list.add("no");
+		list.add("ha");
+		list.add("hi");
+		list.add("hu");
+		list.add("fu");
+		list.add("he");
+		list.add("ho");
+		list.add("ma");
+		list.add("mi");
+		list.add("mu");
+		list.add("me");
+		list.add("mo");
+		list.add("ya");
+		list.add("yu");
+		list.add("yo");
+		list.add("ra");
+		list.add("ri");
+		list.add("ru");
+		list.add("re");
+		list.add("ro");
+		list.add("wa");
+		list.add("wo");
+		list.add("n");
+
+		list.add("wi");
+		list.add("we");
+
+		list.add("vu");
+		list.add("ga");
+		list.add("gi");
+		list.add("gu");
+		list.add("ge");
+		list.add("go");
+		list.add("za");
+		list.add("ji");
+		list.add("zu");
+		list.add("ze");
+		list.add("zo");
+		list.add("da");
+		list.add("dji");
+		list.add("dzu");
+		list.add("de");
+		list.add("do");
+		list.add("ba");
+		list.add("bi");
+		list.add("bu");
+		list.add("be");
+		list.add("bo");
+
+		list.add("pa");
+		list.add("pi");
+		list.add("pu");
+		list.add("pe");
+		list.add("po");
+
+		list.add("+a");
+		list.add("+i");
+		list.add("+u");
+		list.add("+e");
+		list.add("+o");
+		list.add("+ka");
+		list.add("+ke");
+		list.add("+tsu");
+		list.add("+ya");
+		list.add("+yu");
+		list.add("+yo");
+		list.add("+wa");
+
+		for (String romaji : Arrays.asList("a", "hi", "bu", "po", "we")) {
+			String low = romaji.toLowerCase();
+			String up = romaji.toUpperCase();
+
+			/*
+			 * Preliminary tests to ensure that modifications above do not
+			 * change the purpose of the test.
+			 */
+			assertTrue("Test corrupted!", Character.isLowerCase(low.charAt(0)));
+			assertTrue("Test corrupted!", Character.isUpperCase(up.charAt(0)));
+
+			assertEquals("For " + romaji + ": ",
+					factory.createFromRomaji(low, KanaType.HIRAGANA),
+					factory.createFromRomaji(up, KanaType.HIRAGANA));
+			assertEquals("For " + romaji + ": ",
+					factory.createFromRomaji(low, KanaType.KATAKANA),
+					factory.createFromRomaji(up, KanaType.KATAKANA));
+		}
+	}
+
+	@Test
+	public void testCreateFromRomajiThrowsExceptionForNullTypes() {
+		KanaFactory factory = new KanaFactory();
+		try {
+			factory.createFromRomaji("A", null);
+			fail("No exception thrown");
+		} catch (NullPointerException | IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	public void testCreateFromRomajiThrowsExceptionForNullString() {
+		KanaFactory factory = new KanaFactory();
+		try {
+			factory.createFromRomaji(null, KanaType.HIRAGANA);
+			fail("No exception thrown");
+		} catch (NullPointerException | IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	public void testCreateFromRomajiThrowsExceptionForEmptyString() {
+		KanaFactory factory = new KanaFactory();
+		try {
+			factory.createFromRomaji("", KanaType.HIRAGANA);
+			fail("No exception thrown");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	public void testCreateFromRomajiThrowsExceptionForInvalidString() {
+		KanaFactory factory = new KanaFactory();
+		try {
+			factory.createFromRomaji("kya", KanaType.HIRAGANA);
+			fail("No exception thrown");
+		} catch (IllegalArgumentException e) {
+		}
+	}
 
 	@Test
 	public void testTransformReturnsSelfWithSameVariant() {
